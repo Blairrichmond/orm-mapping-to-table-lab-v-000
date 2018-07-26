2fec5b9954862578d1175150dbeab417faa5dd45
Brent  Ransom <sadumedusa@gmail.com>
2018-07-26 19:44:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,4 +34,10 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
+  def self.save(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
